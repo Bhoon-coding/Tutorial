0275f6298fd51df0646c4678128c5592d1ee15b2
이병훈 <bhlee@onns.co.kr>
2022-02-14 17:42:38 +0900

    AppDelegate 라이프사이클 기록

--- Tutorial/AppDelegate.swift
+++ Tutorial/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,7 +15,88 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print(
+              """
+              --------------------------------------------------
+              didFinishLaunchingWithOptions:
+              - 앱이 실행된 직후 사용자의 화면에 보여지기 직전에 호출
+              --------------------------------------------------
+              """
+        )
         return true
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print(
+              """
+              --------------------------------------------------
+              willFinishLaunchingWithOptions:
+              - 앱이 최초 실행될 때 호출
+              --------------------------------------------------
+              """
+        )
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print(
+              """
+              --------------------------------------------------
+              [AppDelegate]
+              applicationWillResignActive:
+              - inactive 상태로 되기전에 호출
+              --------------------------------------------------
+              """
+        )
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print(
+              """
+              --------------------------------------------------
+              [AppDelegate]
+              applicationDidEnterBackground:
+              - 앱이 백그라운드로 전환된 후 호출
+              --------------------------------------------------
+              """
+        )
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print(
+              """
+              --------------------------------------------------
+              [AppDelegate]
+              applicationWillEnterForeground:
+              - 앱이 Foreground로 진입하기 전에 호출
+              - active 상태가 되기전 호출
+              --------------------------------------------------
+              """
+        )
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print(
+              """
+              --------------------------------------------------
+              [AppDelegate]
+              applicationDidBecomeActive:
+              - active 상태가 된 후 호출
+              --------------------------------------------------
+              """
+        )
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print(
+              """
+              --------------------------------------------------
+              [AppDelegate]
+              applicationWillTerminate:
+              - 앱이 종료되기 직전에 호출
+              --------------------------------------------------
+              """
+        )
     }
 
     // MARK: UISceneSession Lifecycle
