683c45344fbc5767d9d73f40af9d5ee23964fc2d
이병훈 <bhlee@onns.co.kr>
2022-03-03 14:20:12 +0900

    touchesBegan 메소드를 활용한 keyboard hide 기능

--- Tutorial/Helper/Extension.swift
+++ Tutorial/Helper/Extension.swift
@@ -5,16 +5,16 @@
 //  Created by BH on 2022/02/27.
 //
 
-import Foundation
-import UIKit
-
-extension UIViewController {
-    func hideKeyboard() {
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self,
-                                                         action: #selector(UIViewController.dismissKeyboard)))
-    }
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-}
+//import Foundation
+//import UIKit
+//
+//extension UIViewController {
+//    func hideKeyboard() {
+//        view.addGestureRecognizer(UITapGestureRecognizer(target: self,
+//                                                         action: #selector(UIViewController.dismissKeyboard)))
+//    }
+//    
+//    @objc func dismissKeyboard() {
+//        view.endEditing(true)
+//    }
+//}
