b7ebf05e675a528fb94e022550018e989f9abec3
이병훈 <bhlee@onns.co.kr>
2022-03-16 16:43:09 +0900

    URLSession 폴더 및 구조 생성

---
+++ Tutorial/URLSession/MovieService.swift
@@ -0,0 +1,14 @@
+//
+//  MovieService.swift
+//  Tutorial
+//
+//  Created by BH on 2022/03/16.
+//
+
+import Foundation
+
+struct MovieService {
+    static let shared = MovieService()
+    
+    
+}
---
+++ Tutorial/URLSession/ViewController/URLSessionController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieViewController.swift
+//  Tutorial
+//
+//  Created by BH on 2022/03/16.
+//
+
+import UIKit
+
+class UrlSessionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
