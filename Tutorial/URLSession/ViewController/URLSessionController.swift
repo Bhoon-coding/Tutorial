7d7f4ecf18bed9d8ec206e9bcef42b61541458f9
이병훈 <bhlee@onns.co.kr>
2022-03-17 15:34:39 +0900

    MovieService (fetchMovieData) 구현

--- Tutorial/URLSession/ViewController/URLSessionController.swift
+++ Tutorial/URLSession/ViewController/URLSessionController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class UrlSessionViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    var movieList: [Movie] = []
 
+    // MARK: LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
+    // MARK: Methods
+    
+    func getMovieData() {
+        MovieService.shared.fetchMovieData { (response) in
+            switch response {
+            case .success(let movieData):
+                if let decodedData = movieData as? MovieModel {
+                    self.movieList = decodedData.items
+//                    DispatchQueue.main.async {
+//                        <#code#>
+//                    }
+                    return
+                }
+            case .failure(let movieData):
+                print("fail", movieData)
+            }
+        }
+    }
 
 }
