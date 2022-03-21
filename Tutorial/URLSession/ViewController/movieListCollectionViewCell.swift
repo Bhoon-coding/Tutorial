e740e3e8cf6cb71628010a39918908cec87193ea
이병훈 <bhlee@onns.co.kr>
2022-03-21 16:34:58 +0900

    API clientID , clientSecret 키 암호화 및 폴더 구조 변경 (APIConstant)

--- Tutorial/URLSession/ViewController/movieListCollectionViewCell.swift
+++ Tutorial/URLSession/ViewController/movieListCollectionViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 import SnapKit
 
-class movieListCollectionViewCell: UICollectionViewCell {
+class MovieListCollectionViewCell: UICollectionViewCell {
     
     // MARK: Properties
     
