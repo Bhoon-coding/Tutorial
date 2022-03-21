24b222de52c560aacddb10e7d16c5a0968091233
이병훈 <bhlee@onns.co.kr>
2022-03-21 11:17:21 +0900

    collectionViewCell 이미지 보여주기 구현 (url -> data)

---
+++ Tutorial/Helper/APIConstant.swift
@@ -0,0 +1,15 @@
+//
+//  APIConstant.swift
+//  Tutorial
+//
+//  Created by BH on 2022/03/16.
+//
+
+import Foundation
+
+struct APIConstant {
+    
+    static let clientID = "kUaKLDAe2wsIGnsZa6C8"
+    static let clientSecret = "DHItoIa8a8"
+    
+}
